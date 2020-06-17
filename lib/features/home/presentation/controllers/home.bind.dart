211f664dabef212fba15060c2681c4a4d2a5163d
speduardo <eduardo.souzapereira@gmail.com>
2020-06-17 13:00:53 -0300

    Refactoring hive

--- lib/features/home/presentation/controllers/home.bind.dart
+++ lib/features/home/presentation/controllers/home.bind.dart
@@ -1,6 +1,7 @@
 import 'package:fluttercleanarchitecture/features/home/data/datasources/building.datasource.dart';
 import 'package:fluttercleanarchitecture/features/home/data/repositories/building.repository.dart';
 import 'package:fluttercleanarchitecture/features/home/domain/usecases/building.usecase.dart';
+import 'package:fluttercleanarchitecture/features/home/presentation/controllers/building.controller.dart';
 import 'package:fluttercleanarchitecture/features/home/presentation/controllers/home.controller.dart';
 import 'package:get/get.dart';
 
@@ -8,6 +9,7 @@ class HomeBind extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeController>(() => HomeController(BuildingUseCase(repository: BuildingRepository(dataSource: BuildingDataSource()))));
+    Get.lazyPut<BuildingController>(() => BuildingController(BuildingUseCase(repository: BuildingRepository(dataSource: BuildingDataSource()))));
   }
 
   /*String call<SampleBind>() {
